c2ece0a2209ad25aa65ca703efcd7a9419b9c9fd
Fube <fubeitch@gmail.com>
2021-03-02 11:56:09 -0500

    Fixed dotenv

--- src/main/kotlin/com/abrari/jokesrating/JokesRatingApplication.kt
+++ src/main/kotlin/com/abrari/jokesrating/JokesRatingApplication.kt
@@ -1,16 +1,17 @@
 package com.abrari.jokesrating
 
 import io.github.cdimascio.dotenv.Dotenv
-import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class JokesRatingApplication
 
+val env: Dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load()
+
 enum class EnvVars(val value:String){
 
-    JOKE_SERVICE_BASE_URL(dotenv().get("JOKE_SERVICE_BASE_URL") ?: "localhost:8080")
+    JOKE_SERVICE_BASE_URL(env["JOKE_SERVICE_BASE_URL"] ?: "localhost:8080")
 }
 
 fun main(args: Array<String>) {
